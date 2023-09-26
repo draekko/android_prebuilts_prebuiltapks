d3845d63b9114e3d36d324e01c06120c8106be74
draekko <draekko.software@gmail.com>
2023-09-26 09:19:02 -0400

    fixed module name call

--- LocalNlpBackend/Android.mk
+++ LocalNlpBackend/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := NominatimNlpBackend
+LOCAL_MODULE := LocalNlpBackend
 LOCAL_SRC_FILES := helium314.localbackend_38.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
